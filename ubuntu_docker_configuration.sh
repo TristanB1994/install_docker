7c740936ea4f34f31321566aa35d3e61d8e07a42
Ubuntu <ubuntu@ip-172-31-7-70.ca-central-1.compute.internal>
2020-09-06 05:41:17 +0000

    update shell script

--- ubuntu_docker_configuration.sh
+++ ubuntu_docker_configuration.sh
@@ -2,7 +2,7 @@
 # Install docker
 sudo apt update
 sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
    $(lsb_release -cs) \
