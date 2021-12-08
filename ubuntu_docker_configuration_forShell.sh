94905be756f244ed64176667d69ea0a5b2ae39fe
Tristan Blandford <t.blandford.dev@gmail.com>
2021-12-08 13:54:36 -0500

    Update ubuntu_docker_configuration_forShell.sh

--- ubuntu_docker_configuration_forShell.sh
+++ ubuntu_docker_configuration_forShell.sh
@@ -12,7 +12,7 @@ sudo apt install -y docker-ce
 sudo usermod -aG docker ${USER}
 
 # Install docker-compose
-sudo curl -L https://github.com/docker/compose/releases/download/1.21.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+sudo curl -L https://github.com/docker/compose/releases/download/1.29.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
 less /var/log/cloud-init-output.log
