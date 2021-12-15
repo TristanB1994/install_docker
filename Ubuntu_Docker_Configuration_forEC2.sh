815521ba9b15235e204d34c33cdc15a1a7e1ecdb
Tristan Blandford <t.blandford.dev@gmail.com>
2021-12-15 11:07:20 -0500

    Update Ubuntu_Docker_Configuration_forEC2.sh

--- Ubuntu_Docker_Configuration_forEC2.sh
+++ Ubuntu_Docker_Configuration_forEC2.sh
@@ -12,7 +12,7 @@ apt-get install -y docker-ce
 usermod -aG docker ubuntu
 
 # Install docker-compose
-curl -L https://github.com/docker/compose/releases/download/1.21.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+curl -L https://github.com/docker/compose/releases/download/1.29.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 
 less /var/log/cloud-init-output.log
